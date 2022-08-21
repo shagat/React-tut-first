62ababc8aedbfc9410e9ffb34c5193ddabc39e72
shagat <shagat.bing@gmail.com>
2022-08-21 21:34:11 +0530

    added card and also added meetup list and items

--- src/components/meetups/MeetupItem.js
+++ src/components/meetups/MeetupItem.js
@@ -1,4 +1,23 @@
-function MeetupItem(){
-    return
+import classes from "./MeetupItem.module.css";
+import React from "react";
+import Card from "../ui/Card";
+function MeetupItem(props) {
+  return (
+    <li className={classes.item}>
+      <Card>
+        <div className={classes.image}>
+          <img src={props.image} alt={props.title} />
+        </div>
+        <div className={classes.content}>
+          <h3>{props.title}</h3>
+          <address> {props.address}</address>
+          <p>{props.description}</p>
+        </div>
+        <div className={classes.actions}>
+          <button>To Favourite</button>
+        </div>
+      </Card>
+    </li>
+  );
 }
 export default MeetupItem;
