582a16a917f2e2656918903d38a56a9c8a9e8e66
shagat <shagat.bing@gmail.com>
2022-08-21 22:00:58 +0530

    added forms UI

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,5 +1,11 @@
 import React from "react";
-function NewMeetupPage(){
-    return <div>New Meetups Page</div>
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
+function NewMeetupPage() {
+  return (
+    <section>
+      <h1>Add new meetup</h1>
+      <NewMeetupForm />
+    </section>
+  );
 }
 export default NewMeetupPage;
